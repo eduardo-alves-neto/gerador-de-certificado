1de5edc91ec7c5e7818d268e4273167eb83a9dc7
eduardo-alves-neto <emailforprojects01@gmail.com>
2025-06-11 19:43:05 -0300

    feat: add certificate form screen

--- src/app/_components/certificado-form/certificado-form.component.ts
+++ src/app/_components/certificado-form/certificado-form.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [],
+  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
   templateUrl: './certificado-form.component.html',
-  styleUrl: './certificado-form.component.css'
+  styleUrl: './certificado-form.component.css',
 })
-export class CertificadoFormComponent {
-
-}
+export class CertificadoFormComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './_components/navbar/navbar.component';
-import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
-import { ItemCerticadoComponent } from "./_components/item-certicado/item-certicado.component";
-import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CetificadosComponent } from "./pages/cetificados/cetificados.component";
+import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
+import { ItemCerticadoComponent } from './_components/item-certicado/item-certicado.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { CetificadosComponent } from './pages/cetificados/cetificados.component';
+import { CertificadoFormComponent } from './_components/certificado-form/certificado-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCerticadoComponent, BaseUiComponent, CetificadosComponent],
+  imports: [
+    NavbarComponent,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    ItemCerticadoComponent,
+    BaseUiComponent,
+    CetificadosComponent,
+    CertificadoFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
