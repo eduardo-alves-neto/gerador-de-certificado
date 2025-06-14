049efa646f80ea059db126515f085853ddf800e0
eduardo-alves-neto <emailforprojects01@gmail.com>
2025-06-14 10:25:13 -0300

    feat: adding inputs to the buttons

--- src/app/_components/certificado-form/certificado-form.component.ts
+++ src/app/_components/certificado-form/certificado-form.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
-import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  imports: [
+    SecondaryButtonComponent,
+    PrimaryButtonComponent,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css',
 })
-export class CertificadoFormComponent {}
+export class CertificadoFormComponent {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React', 'Cobol'];
+
+  campoInValido(control: NgModel): boolean {
+    return (control.invalid && control.touched) || false;
+  }
+
+  formValido(): boolean {
+    return this.atividades.length > 0 && this.nome.length > 0;
+  }
+}
--- src/app/pages/cetificados/cetificados.component.ts
+++ src/app/pages/cetificados/cetificados.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
-import { ItemCerticadoComponent } from "../../_components/item-certicado/item-certicado.component";
+import { ItemCerticadoComponent } from '../../_components/item-certicado/item-certicado.component';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cetificados',
-  imports: [ItemCerticadoComponent],
+  imports: [ItemCerticadoComponent, SecondaryButtonComponent, RouterLink],
   templateUrl: './cetificados.component.html',
-  styleUrl: './cetificados.component.css'
+  styleUrl: './cetificados.component.css',
 })
-export class CetificadosComponent {
- 
-}
+export class CetificadosComponent {}
